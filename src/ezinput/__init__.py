7eba6f617cdb5779ec4f971b401ec817e4f61956
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-03-25 10:50:24 +0000

    adding more docstrings

--- src/ezinput/__init__.py
+++ src/ezinput/__init__.py
@@ -1,3 +1,20 @@
+
+
 from .ezinput_jupyter import EZInputJupyter
+"""
+EZInput Package
+
+This package provides a set of tools for simplifying user input handling in Python applications. 
+It includes modules for managing input in different environments, such as Jupyter notebooks 
+and command-line prompts, as well as a general-purpose input handler.
+
+Modules:
+- ezinput_jupyter: Handles user input specifically in Jupyter notebook environments.
+- ezinput_prompt: Provides utilities for managing command-line input prompts.
+- ezinput: A general-purpose input handler for various use cases.
+
+The EZInput package is designed to streamline the process of gathering and managing user input, 
+making it easier to integrate into Python projects.
+"""
 from .ezinput_prompt import EZInputPrompt
 from .ezinput import EZInput
